5160923a655b19aa238158c784b260bb5166f47f
Mattullon <mattullon@gmail.com>
2023-01-06 19:32:19 -0300

    buena

---
+++ 01-reforzamiento/src/App.tsx
@@ -0,0 +1,22 @@
+import TiposBasicos from "./Typescript/tiposBasicos"
+  
+
+
+let nombre : string  = "matias"
+  
+
+
+console.log(nombre)
+
+
+ const App = () => {
+  return (
+    <div className="mt-2">
+      <h1> introduccion a TS </h1>
+      <hr/>
+      <TiposBasicos/>
+    </div>
+  )
+}
+
+export default App
---
+++ 01-reforzamiento/src/Typescript/tiposBasicos.tsx
@@ -0,0 +1,10 @@
+
+const TiposBasicos = () => {
+  return (
+    <>
+    <h1> hola perros </h1>
+    </>
+  )
+}
+
+export default TiposBasicos;
